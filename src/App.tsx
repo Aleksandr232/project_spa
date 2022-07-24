7affaa30f9a461d9cd2d7616330503e823cc1d78
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-24 21:25:57 +0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Route, Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Main } from './components/pages/main';
+import { Auth } from './components/pages/auth';
+import { Airoport } from './components/pages/airport';
 
 
 function App() {
   return (
     <div>
-      <Router>
-          <Route path=''/>
-          <Route path=''/>
-          <Route path=''/>
-      </Router>
+      <Routes>
+          <Route path='/' element={<Main/>}/>
+          <Route path='/auth' element={<Auth/>}/>
+          <Route path='/airport:/id' element={<Airoport/>}/>
+      </Routes>
     </div>
   );
 }
