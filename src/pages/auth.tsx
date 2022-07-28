67a87fef008a840657ee254a9be093c262053d43
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-28 11:25:45 +0300

    auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -3,7 +3,23 @@ import React from "react";
 
 
 export function Auth(){
+
+    const sumbitHandler=(event: React.FormEvent)=>{
+        event.preventDefault()
+    }
+
     return(
-        <div>auth</div>
+        <form className="container mx-auto max-w-[500px] pt-8"
+            onSubmit={sumbitHandler}
+        >
+            <div className="mb-2">
+                <label htmlFor="username" className="block">Имя пользователя</label>
+                <input type="text" className="border py-1 px-2 w-full"  value={11} id='username'/>
+            </div>
+            <div className="mb-2">
+                <label htmlFor="password" className="block">Пароль пользователя</label>
+                <input type="text" className="border py-1 px-2 w-full"  value={11} id='username'/>
+            </div>
+        </form>
     )
 }
