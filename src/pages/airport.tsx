bb1444bc72f1e85432d18c02cdf6f55d08381fd5
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-28 10:55:13 +0300

    airport fetch

--- src/pages/airport.tsx
+++ src/pages/airport.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { useParams } from "react-router-dom";
+import axios from "../axios";
 
 
 export function Airoport(){
     const params = useParams<'id'>()
+
+    async function fetcAirport(){
+        await axios.get(`/airports/${params.id}`)
+    }
+
+    useEffect(()=>{
+        fetcAirport()
+    },[])
+
+  
+
     return(
-        <div className="container mx-auto pt-5 max-w[760px]">
+        <div className="container mx-auto pt-5 max-w-[760px]">
             <h1>Аэропорт {params.id}</h1>
         </div>
     )
